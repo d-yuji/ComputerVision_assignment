3ab70afdd254d06b09fa002a00c2660c8125e4bd
YujiDouken <d.yuji.fm@gmail.com>
2017-06-15 17:12:44 +0900

    add comment

--- main.cpp
+++ main.cpp
@@ -9,67 +9,66 @@ using namespace cv;
 
 int main(int argc, char *argv[]) {
 
-  // Check arguments
-  if( argc != 3 ){
-    cout << "Usage: ./run /path/to/the/dataset/ lv" << endl;
-    exit(1);
-  }
+	// Check arguments
+	if( argc != 3 ){
+		cout << "Usage: ./run /path/to/the/dataset/ lv" << endl;
+		exit(1);
+	}
 
-  // データセットのパスを設定する
-  const string dirData = argv[1]; // Path to the dataset
-  const string lv = argv[2]; //レベルの指定
-  
-  // アノテーションファイルを指定します
-  const string fn_train_target = "target_lv" + lv + "_train_1.csv";
-  const string fn_test_target = "target_lv" + lv + "_test_1.csv";
-  const string fn_train_ground = "groundtruth_lv" + lv + "_train_1.csv";
-  const string fn_test_ground = "groundtruth_lv" + lv + "_test_1.csv"; 
+	// データセットのパスを設定する
+	const string dirData = argv[1]; // Path to the dataset
+	const string lv = argv[2]; //レベルの指定
+	
+	// アノテーションファイルを指定します
+	const string fn_train_target = "target_lv" + lv + "_train_1.csv";
+	const string fn_test_target = "target_lv" + lv + "_test_1.csv";
+	const string fn_train_ground = "groundtruth_lv" + lv + "_train_1.csv";
+	const string fn_test_ground = "groundtruth_lv" + lv + "_test_1.csv"; 
+	
+	// 学習データを読み込む
+	string fnTrainTar = dirData + "annotations/train_lv1/" + fn_train_target;
+	string fnTrainGrn = dirData + "annotations/train_lv1/" + fn_train_ground;
+	CAlconDatabase traindata( dirData, fnTrainTar, fnTrainGrn );
 
-  
-  // 学習データを読み込む
-  string fnTrainTar = dirData + "annotations/train_lv1/" + fn_train_target;
-  string fnTrainGrn = dirData + "annotations/train_lv1/" + fn_train_ground;
-  CAlconDatabase traindata( dirData, fnTrainTar, fnTrainGrn );
+	// テストデータを読み込む
+	string fnTestTar = dirData + "annotations/test_lv1/" + fn_test_target;
+	string fnTestGrn = dirData + "annotations/test_lv1/" + fn_test_ground;
+	CAlconDatabase testdata( dirData, fnTestTar, fnTestGrn );
 
-  // テストデータを読み込む
-  string fnTestTar = dirData + "annotations/test_lv1/" + fn_test_target;
-  string fnTestGrn = dirData + "annotations/test_lv1/" + fn_test_ground;
-  CAlconDatabase testdata( dirData, fnTestTar, fnTestGrn );
+	// ターゲットごとに認識します
+	CMyAlgorithm myAlg;//自分でアルゴリズムを作るクラス
 
-  // ターゲットごとに認識します
-  CMyAlgorithm myAlg;//自分でアルゴリズムを作るクラス
+	vector<int> ids = testdata.GetIDs(); // テストデータのID
+	map< int, vector<string> > outs; // すべての出力結果. map[ key ] = vector<string>
 
-  vector<int> ids = testdata.GetIDs(); // テストデータのID
-  map< int, vector<string> > outs; // すべての出力結果. map[ key ] = vector<string>
+	// テストデータごとにアルゴリズムを適用
+	for( vector<int>::iterator it = ids.begin(); it!=ids.end(); it++ ){
+		
+		// 認識対象のデータを取得する
+		CTarget targetData = testdata.GetTarget( *it );
+		//t.PrintMembers();
 
-  // テストデータごとにアルゴリズムを適用
-  for( vector<int>::iterator it = ids.begin(); it!=ids.end(); it++ ){
-    
-    // 認識対象のデータを取得する
-    CTarget t = testdata.GetTarget( *it );
-    //t.PrintMembers();
+		// 自分のアルゴリズムを起動する
+		vector<string> outsResult = myAlg.exe( targetData, traindata );
 
-    // 自分のアルゴリズムを起動する
-    vector<string> o = myAlg.exe( t, traindata );
+		// 結果を表示する
+		cout << "Output: target[" << *it << "] = ";
+		
+		for(vector<string>::iterator it2 = outsResult.begin(); it2 != outsResult.end(); it2++){
+			cout << *it2 << ",";
+		}
+		cout << endl;
+		
+		// 出力結果を格納する
+		outs[ targetData.GetID() ] = outsResult;
 
+	}
 
-    // 結果を表示する
-    cout << "Output: target[" << *it << "] = ";
-    for(vector<string>::iterator it2=o.begin(); it2!=o.end(); it2++){
-      cout << *it2 << ",";
-    }
-    cout << endl;
-    
-    // 出力結果を格納する
-    outs[ t.GetID() ] = o;
-
-  }
-
-  // Evaluation
-  string fnOuts = "myOutputs" + lv + ".csv";
-  testdata.WriteOutputs( outs, fnOuts );
-  testdata.Evaluation( outs );
-  
-  return 0;
+	// Evaluation
+	string fnOuts = "myOutputs" + lv + ".csv";
+	testdata.WriteOutputs( outs, fnOuts );
+	testdata.Evaluation( outs );
+	
+	return 0;
 }
 
--- myAlgorithm.cpp
+++ myAlgorithm.cpp
@@ -17,26 +17,34 @@ vector<string> CMyAlgorithm::exe( CTarget target, CAlconDatabase traindata ){
 	// ターゲットの特徴抽出
 	Mat fTest = extractFeatures( imgTest );
 
+// ここら辺のアルゴリズムを変えて考える
+	
 	// NN法で認識する
 	vector<int> ids = traindata.GetIDs();
 	double dmin = DBL_MAX;
 	int idMin;
+
 	for( vector<int>::iterator it=ids.begin(); it!=ids.end(); it++ ){
 		// 学習データとの距離を計算する
 		CTarget tr = traindata.GetTarget( *it ); // 学習データのターゲット
 		Mat imgTrain = tr.GetTargetImage();
 		Mat fTrain = extractFeatures( imgTrain ); // 学習データの特徴
 		double d = norm( fTest - fTrain ); // L2ノルム
-		// 最小値
-		if( dmin > d ){ dmin = d; idMin = tr.GetID(); }
+		
+		// 距離を比較してその最小値を、文字の種類とする
+		if( dmin > d ){
+			dmin = d;
+			idMin = tr.GetID();
+		}
 	}
 
-	// 認識結果
+	// 認識結果をベクトルとして返す
 	vector<string> output = traindata.GetUnicodesForOneTarget( idMin );
 	return output;
 }
 
 
+// 特徴抽出を行うメソッド
 Mat CMyAlgorithm::extractFeatures( Mat img ){
 	cvtColor( img, img, CV_RGB2GRAY ); // グレースケール変換
 	resize( img, img, Size(32,32), 0, 0 ); // Resize
